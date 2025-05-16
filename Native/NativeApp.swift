779ea0a5ae99cd4e97bc14c1c587d75a3f481b20
Christian Okeke <okekec21@gmail.com>
2025-05-16 14:51:25 -0500

    Initial Commit

---
+++ Native/NativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativeApp.swift
+//  Native
+//
+//  Created by Christian Okeke on 5/16/25.
+//
+
+import SwiftUI
+
+@main
+struct NativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
